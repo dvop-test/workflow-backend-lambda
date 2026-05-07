dcf59f9f4aaf5a8a7e8e1978871f001ad10de59f
jean.lizano <jclizano01@gmail.com>
2026-05-07 00:44:57 -0500

    wip: CI 9

--- makefiles/fn.mk
+++ makefiles/fn.mk
@@ -3,8 +3,16 @@
 ## FUNTIONS ##
 define create.zip
 	@echo "---- Empaquetando desde $(LAMBDA_ROOT) ----"
-	ls -la $(LAMBDA_ROOT)/dist
 	cd $(LAMBDA_ROOT)/dist && zip -j lambda.zip index.js
+	@if [ ! -f $(LAMBDA_ROOT)/dist/lambda.zip ]; then \
+		echo "::error::Error: No se encontró el archivo lambda.zip tras la ejecución."; \
+		exit 1; \
+	fi
+	@if [ ! -s $(LAMBDA_ROOT)/dist/lambda.zip ]; then \
+		echo "::error::Error: El archivo lambda.zip está vacío (0 bytes)."; \
+		exit 1; \
+	fi
+	@echo "——— Validación exitosa: Archivo generado correctamente ———"
 endef
 
 # ##################################################################
