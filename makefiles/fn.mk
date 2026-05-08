34f4d86a28fa84bf300d86a369667304ac145d5e
jean.lizano <jclizano01@gmail.com>
2026-05-08 15:00:13 -0500

    wip: CI 44

--- makefiles/fn.mk
+++ makefiles/fn.mk
@@ -1,18 +1,18 @@
 .PHONY: update.lambda \
 
 ## GENERAL ##
-PROJECT_NAME_ZIP     			= ${PROJECT_NAME}-${APP_ENV}
+PROJECT_NAME_ZIP     			= ${PROJECT_NAME}-${APP_ENV}.zip
 
 ## FUNTIONS ##
 define create.zip
 	@echo "---- Empaquetando desde $(PROJECT_DIR) ----"
-	cd $(PROJECT_DIR)/dist && zip -j ${PROJECT_NAME_ZIP}.zip index.js
-	@if [ ! -f $(PROJECT_DIR)/dist/${PROJECT_NAME_ZIP}.zip ]; then \
-		echo "::error::Error: No se encontró el archivo ${PROJECT_NAME_ZIP}.zip tras la ejecución."; \
+	cd $(PROJECT_DIR)/dist && zip -j ${PROJECT_NAME_ZIP} index.js
+	@if [ ! -f $(PROJECT_DIR)/dist/${PROJECT_NAME_ZIP} ]; then \
+		echo "::error::Error: No se encontró el archivo ${PROJECT_NAME_ZIP} tras la ejecución."; \
 		exit 1; \
 	fi
-	@if [ ! -s $(PROJECT_DIR)/dist/${PROJECT_NAME_ZIP}.zip ]; then \
-		echo "::error::Error: El archivo ${PROJECT_NAME_ZIP}.zip está vacío (0 bytes)."; \
+	@if [ ! -s $(PROJECT_DIR)/dist/${PROJECT_NAME_ZIP} ]; then \
+		echo "::error::Error: El archivo ${PROJECT_NAME_ZIP} está vacío (0 bytes)."; \
 		exit 1; \
 	fi
 	@echo "——— Validación exitosa: Archivo generado correctamente ———"
